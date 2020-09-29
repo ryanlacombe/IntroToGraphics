01c71c3131a1e3f4230f97af0bfa3fc3b415372a
Rain <ryanalacombe@gmail.com>
2020-09-29 14:00:49 -0500

    Started work with Gizmos

--- GraphicsScene/main.cpp
+++ GraphicsScene/main.cpp
@@ -1,6 +1,13 @@
 #include <gl_core_4_4.h>
 #include <GLFW/glfw3.h>
+#include <Gizmos.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
 #include <cstdio>
+
+using glm::vec3;
+using glm::vec4;
+using glm::mat4;
 
 int main() 
 {
@@ -30,11 +37,25 @@ int main()
 	int minor = ogl_GetMinorVersion();
 	printf("GL: %i.%i\n", major, minor);
 
+	aie::Gizmos::create(10000, 10000, 10000, 10000);
+
+	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
+	mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.0f, 0.1f, 1000.0f);
+
+	glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+
 	while (!glfwWindowShouldClose(window) && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
 	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	aie::Gizmos::destroy();
+
 	glfwDestroyWindow(window);
 
 	glfwTerminate();
